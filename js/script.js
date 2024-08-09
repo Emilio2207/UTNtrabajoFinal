02e91d22c201d3dcf6eedc5c4242ec395438f4a3
EmilioRamirez <emi.emilioramirez@gmail.com>
2024-08-09 17:08:40 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* scroll */
 const change = () => {
   const { innerHeight, scrollY } = window;
   const nav = document.querySelector(".change-nav");
@@ -10,3 +11,40 @@ const change = () => {
 };
 
 window.addEventListener("scroll", change);
+
+/* logo */
+document.addEventListener("DOMContentLoaded", function () {
+  const imgElement = document.getElementById("logoCompu");
+  const svgUrl = imgElement.getAttribute("src");
+
+  fetch(svgUrl)
+    .then((response) => response.text())
+    .then((svgContent) => {
+      const svgContainer = document.createElement("div");
+      svgContainer.innerHTML = svgContent;
+      const svgElement = svgContainer.querySelector("svg");
+
+      imgElement.replaceWith(svgElement);
+
+      animateSVG(svgElement);
+    })
+    .catch((error) => console.error("Error cargando el SVG:", error));
+});
+
+function animateSVG(svg) {
+  const elementsNoteA = svg.querySelectorAll(".noteA");
+  const elementsNoteB = svg.querySelectorAll(".noteB");
+  const elementsLog = svg.querySelectorAll(".log");
+
+  elementsNoteA.forEach((element) => {
+    element.style.animation = "sec_A ease-in 5s 1";
+  });
+
+  elementsNoteB.forEach((element) => {
+    element.style.animation = "sec_B ease-in 5s 1";
+  });
+
+  elementsLog.forEach((element) => {
+    element.style.animation = "logType ease-in 2s 1 forwards";
+  });
+}
